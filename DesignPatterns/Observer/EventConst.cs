79f5989990531717c92d049e5dd43dea78c06279
chuozt <hai09032004@gmail.com>
2025-10-13 17:48:59 +0700

    Delete event consts

--- DesignPatterns/Observer/EventConst.cs
+++ DesignPatterns/Observer/EventConst.cs
@@ -2,7 +2,6 @@ namespace GameCore.Observers
 {
     public static class EventConst
     {
-        public const string ON_HEALTH_CHANGED = "OnHealthChanged";
-        public const string ON_MANA_CHANGED = "OnManaChanged";
+        
     }
 }
